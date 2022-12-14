47061de1d7272565e36c7cf9d4d09c1af330fbb4
guojingwen <guojingwen@xiaoyouzi.com>
2022-12-14 21:28:04 +0800

    'add'

--- push/push.py
+++ push/push.py
@@ -1,13 +1,27 @@
 import PySimpleGUI as sg
 layout = [
-    [sg.Text('选择发帖类型'), sg.Radio('图文贴','Type', default=True),sg.Radio('问答贴','Type'),sg.Radio('视频贴','Type')],
-    [sg.Text('输入发帖标题'), sg.InputText(size=(50,10)),sg.Text(size=(40,3),key='output')],
-    [sg.Text('输入发帖内容'), sg.Multiline(size=(50,10),default_text='内容最少输入八个字'),sg.Text(size=(50,10),key='output')],
-    [sg.Text('选择图片张数'), sg.Combo(['1张', '2张', '3张', '4张', '5张', '6张', '7张', '8张'], size=(10, 5),readonly=False)],
-    # [sg.Combo(['1张', '2张', '3张', '4张', '5张', '6张', '7张', '8张'], default_value='1张')],
-    [sg.Button('发布')]
+    [sg.Text('选择发帖类型'), sg.Radio('图文贴','Type', default=True,key='-T-'),sg.Radio('问答贴','Type',key='-Q-'),sg.Radio('视频贴','Type',key='-V-')],
+    [sg.Text('输入发帖标题'), sg.InputText(key='-Title1-',size=(50,20))],
+    [sg.Text('输入发帖内容'), sg.Multiline(key='-Content-',size=(50,10),default_text='内容最少输入八个字')],
+    [sg.Text('选择图片张数'), sg.Combo(['1张', '2张', '3张', '4张', '5张', '6张', '7张', '8张'], size=(10, 5),readonly=False,key='-PictureNum-')],
+    [sg.Button('发布',key='-Publish-')]
 ]
 
-window = sg.Window('发帖小工具',layout)
-window.read()
-window.close
+windows = sg.Window('发帖小工具',layout)
+# while True:
+#     event, values = windows.read()
+#     if event == sg.WIN_CLOSED:
+#         windows.close()
+#         break
+#     if event == "发布":
+#         print(windows['标题'].get())
+#     if event == "图文":
+#         windows["视频"].SetTooltip("哈哈哈")
+#         print("111")
+event,values = windows.read()
+windows.close()
+print(values[0])
+
+
+
+
